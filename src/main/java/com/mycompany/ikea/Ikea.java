5168542fdb670ea80152e70f0bcf386eee35bb9a
edls <edls@comcast.net>
2024-09-20 13:46:41 -0400

    my first commit

---
+++ src/main/java/com/mycompany/ikea/Ikea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ikea;
+
+/**
+ *
+ * @author edls3
+ */
+public class Ikea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
