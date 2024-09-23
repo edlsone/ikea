f4d86fcb3710bb2e776a318d4f2b9cb3e9f267f4
edls <edls@comcast.net>
2024-09-23 17:46:44 -0400

    Fixed wait bug

--- IKEA/src/test/java/test/com/itexps/LoginTest.java
+++ IKEA/src/test/java/test/com/itexps/LoginTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
  */
-package test.com.mycompany.ikea;
+package test.com.itexps;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
