e6390a337f9db29eb0c4a60de9b4dba6f1e76d9a
EricC18 <eric.lewis.carter@gmail.com>
2024-09-23 14:46:16 -0500

    Scroll bar test
    works

---
+++ IKEA/src/main/java/com/mycompany/ikea/IKEA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ikea;
+
+/**
+ *
+ * @author ericl
+ */
+public class IKEA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
